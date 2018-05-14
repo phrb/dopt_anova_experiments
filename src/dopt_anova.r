1b78c7bcdce5a022cbe011c227ee64cf4b82088f
Pedro Bruel <pedro.bruel@gmail.com>
2018-05-14 14:28:58 +0200

    Generate accuracy counts for each step

--- src/dopt_anova.r
+++ src/dopt_anova.r
@@ -1,9 +1,20 @@
 library(AlgDesign)
 library(dplyr)
 
+check_prediction <- function(regression, prediction, target_size) {
+  predicted_effects <- summary(regression)[[1]][["Pr(>F)"]]
+  names(predicted_effects) <- trimws(rownames(summary(regression)[[1]]))
+  predicted_effects <- data.frame(t(predicted_effects), check.names = FALSE)
+
+  ordered_effects <- names(predicted_effects[ , order(predicted_effects[1, ])])
+  str(prediction %in% ordered_effects[1:target_size])
+  return(prediction %in% ordered_effects[1:target_size])
+}
+
 complete_data = read.csv("../data/search_space.csv", header = TRUE)
 # str(complete_data)
 
+accuracies_file <- "../data/dopt_accuracies.csv"
 read_file <- "../data/old_complete_1000.csv"
 write_file <- "../data/testing_1000.csv"
 
@@ -11,12 +22,14 @@ results <- read.csv(read_file, strip.white=T, header=T)
 
 budget <- 120
 
-iterations <- 1000
+iterations <- 500
 
 factors = c("elements_number", "y_component_number",
             "vector_length", "temporary_size",
             "load_overlap", "threads_number",
             "lws_y")
+
+accuracies <- data.frame(first_step = 0, second_step = 0, third_step = 0)
 
 for (i in 1:iterations) {
     str(i)
@@ -57,6 +70,11 @@ for (i in 1:iterations) {
                                        elements_number + I(1 / elements_number) +
                                        threads_number + I(1 / threads_number),
                       data = federov_design)
+
+    if (all(check_prediction(regression, c("vector_length", "lws_y"), 3)) |
+        all(check_prediction(regression, c("vector_length", "I(1/lws_y)"), 3))) {
+        accuracies$first_step <- accuracies$first_step + 1
+    }
 
     used <- used + nrow(federov_design)
 
@@ -100,6 +118,13 @@ for (i in 1:iterations) {
                                        threads_number + I(1 / threads_number),
                       data = federov_design)
 
+    if (all(check_prediction(regression, c("y_component_number", "threads_number"), 4)) |
+        all(check_prediction(regression, c("I(1/y_component_number)", "threads_number"), 4)) |
+        all(check_prediction(regression, c("y_component_number", "I(1/threads_number)"), 4)) |
+        all(check_prediction(regression, c("I(1/y_component_number)", "I(1/threads_number)"), 4))) {
+      accuracies$second_step <- accuracies$second_step + 1
+    }
+
     # print(summary(regression))
 
     # Checking the ANOVA summary we can identify at least two variables
@@ -137,6 +162,11 @@ for (i in 1:iterations) {
     regression <- aov(time_per_pixel ~ load_overlap + temporary_size +
                                        elements_number + I(1 / elements_number),
                       data = federov_design)
+
+    if (all(check_prediction(regression, c("elements_number"), 2)) |
+        all(check_prediction(regression, c("I(1/elements_number)"), 2))) {
+      accuracies$third_step <- accuracies$third_step + 1
+    }
 
     # print(summary(regression))
 
@@ -198,4 +228,7 @@ for (i in 1:iterations) {
     results <- rbind(results, predicted_best)
 }
 
+accuracies <- accuracies / iterations
+
+write.csv(accuracies, accuracies_file, row.names = FALSE)
 write.csv(results, write_file, row.names = FALSE)
